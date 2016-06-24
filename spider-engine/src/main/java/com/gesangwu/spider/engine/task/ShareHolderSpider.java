f9d9f35f24b3707d10061050a5264b71de4e335a
zhuxb <zhuxb@DESKTOP-5GH157O>
2016-06-24 18:52:32 +0800

    添加sql脚本

---
+++ spider-engine/src/main/java/com/gesangwu/spider/engine/task/ShareHolderSpider.java
@@ -0,0 +1,17 @@
+package com.gesangwu.spider.engine.task;
+
+/**
+ * 股东
+ * https://xueqiu.com/S/SH603099/LTGD
+ * 恒生：http://open.hs.net/online-test/arescloud
+ * @author zhuxb
+ *
+ */
+public class ShareHolderSpider {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
