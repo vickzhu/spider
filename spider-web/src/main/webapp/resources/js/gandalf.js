1c6cb502075ce47ff93fed60db514eb0940966ec
zhuxb <zhuxb@DESKTOP-5GH157O>
2016-12-20 18:18:15 +0800

    优化

--- spider-web/src/main/webapp/resources/js/gandalf.js
+++ spider-web/src/main/webapp/resources/js/gandalf.js
@@ -2,6 +2,12 @@ $(document).ready(function(){
 	$('input[type=checkbox],input[type=radio],input[type=file]').uniform();
 	
 	$.ajaxSetup({
+		beforeSend: function(xhr, settings) {
+			if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
+				var csrftoken = jQuery.cookie('_c_t');
+				xhr.setRequestHeader("X-CSRFToken", csrftoken);
+			}
+		},
 		complete:function(xhr, ts){
 			var status = xhr.status;
 			if(status == 200){
@@ -22,4 +28,20 @@ $(document).ready(function(){
 			}
 		}
 	});
+	
+	/*判断是否为csrf安全方法*/
+	function csrfSafeMethod(method) {
+		return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+	}
+	
+	/*判断是否同源*/
+	function sameOrigin(url) {
+	    var host = document.location.host;
+	    var protocol = document.location.protocol;  
+	    var sr_origin = '//' + host;  
+	    var origin = protocol + sr_origin;
+	    return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||  
+	        (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+	        !(/^(\/\/|http:|https:).*/.test(url));  
+	} 
 })
