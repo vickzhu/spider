8bcab50739a7dae7afec8feb5ea3ddacefe201e1
bran <bran@bran-PC>
2018-11-04 22:38:39 +0800

    优化代码

--- spider-web/src/main/java/com/gesangwu/spider/web/velocity/tool/CompanyTool.java
+++ spider-web/src/main/java/com/gesangwu/spider/web/velocity/tool/CompanyTool.java
@@ -4,6 +4,8 @@ import org.apache.velocity.tools.Scope;
 import org.apache.velocity.tools.config.DefaultKey;
 import org.apache.velocity.tools.config.ValidScope;
 
+import com.alibaba.druid.util.StringUtils;
+import com.gandalf.framework.util.StringUtil;
 import com.gandalf.framework.velocity.tool.AbstractTool;
 import com.gesangwu.spider.web.util.StockNameCache;
 
@@ -12,7 +14,8 @@ import com.gesangwu.spider.web.util.StockNameCache;
 public class CompanyTool extends AbstractTool  {
 
 	public static String getStockName(String symbol){
-		return StockNameCache.getStockName(symbol);
+		String companyName = StockNameCache.getStockName(symbol);
+		return StringUtil.isNotBlank(companyName) ? companyName : symbol;
 	}
 	
 }
