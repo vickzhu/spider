97c482087c72a71d89ba82d94715d6040edbcd28
Administrator <Administrator@WINDOWS-9PBBFLV>
2024-07-04 23:25:52 +0800

    升级Mysql驱动

--- spider-engine/src/test/java/com/gesangwu/spider/engine/task/test/CompanyInitTest.java
+++ spider-engine/src/test/java/com/gesangwu/spider/engine/task/test/CompanyInitTest.java
@@ -2,15 +2,18 @@ package com.gesangwu.spider.engine.task.test;
 
 import javax.annotation.Resource;
 
+import org.junit.Test;
+
 import com.gesangwu.spider.engine.task.init.CompanyInit;
 import com.gesangwu.spider.engine.test.BaseTest;
 
+@Deprecated
 public class CompanyInitTest extends BaseTest {
 	
 	@Resource
 	private CompanyInit spider;
 
-//	@Test
+	@Test
 	public void test(){
 		spider.execute();
 	}
